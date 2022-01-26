650f1b479a25b60333352b5ff4c2513d56068aff
Gupodong-seal <Gupodong-seal@DESKTOP-UMLEOPG>
2022-01-26 21:16:08 +0900

    CodeUP 1012

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		float number = scanner.nextFloat();
 		System.out.printf("%f", number);
 		scanner.close();
+		
 	}
 }
